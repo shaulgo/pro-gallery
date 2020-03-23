e1c2d5c0947b84fb86e86ac7d873e28d6632e3c3
ritabazak <rita.bazak@gmail.com>
2020-03-23 19:42:47 +0200

    added more exports that were in use in the PGSW

--- packages/gallery/src/index.js
+++ packages/gallery/src/index.js
@@ -1,8 +1,12 @@
 import './components/styles/gallery.scss';
 
 export { default as ProGallery } from './components/gallery/index';
-export LoveButton from './components/gallery/index';
-export GalleryItem from './components/item/galleryItem';
-export gallerySettings from './settings/merged';
-export ExpandableProGallery from './components/gallery/presets/expandableGallery';
-export GALLERY_CONSTS from './common/constants';
+export { default as LoveButton } from './components/item/loveButton/loveButton';
+export { default as GalleryItem} from './components/item/galleryItem';
+export { default as gallerySettings } from './settings/merged';
+export { default as ExpandableProGallery } from './components/gallery/presets/expandableGallery';
+export { cssScrollHelper } from './components/helpers/cssScrollHelper';
+export { notEligibleReasons } from './components/gallery/leanGallery/isEligible';
+export { addPresetStyles } from './components/gallery/presets/presets';
+export { default as GALLERY_CONSTS } from './common/constants';
+
