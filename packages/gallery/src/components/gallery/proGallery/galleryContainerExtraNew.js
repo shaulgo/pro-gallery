8993349f86a16147d7752558282566a608d2bcec
Guy Sopher <guyso@wix.com>
2020-07-09 12:18:20 +0300

    revive layoutCss in galleryContainerNew

--- packages/gallery/src/components/gallery/proGallery/galleryContainerExtraNew.js
+++ packages/gallery/src/components/gallery/proGallery/galleryContainerExtraNew.js
@@ -641,7 +641,6 @@ export class GalleryContainer extends React.Component {
           </div>
         )}
         <div data-key="items-styles" key="items-styles" style={{display: 'none'}}>
-          {this.layoutCss.map((css, idx) => <style data-key={`layoutCss-${idx}`} key={`layoutCss-${idx}`} dangerouslySetInnerHTML={{__html: css}}/>)}
           {(this.scrollCss || []).filter(Boolean).map((scrollCss, idx) => <style key={`scrollCss_${idx}_${this.allowedPreloading ? 'padded' : 'padless'}`} dangerouslySetInnerHTML={{__html: scrollCss}}/>)}
           {ssrDisableTransition && <style dangerouslySetInnerHTML={{__html: ssrDisableTransition}}/>}
         </div>
